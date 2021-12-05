64025d37abc31ce2ba8d549946f53e8e71963153
Minju Kim <kmj673@gmail.com>
2021-12-05 23:37:35 +0000

    add navigation in each page and set up the minjuKim and projects page

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+let currentPage = document.querySelector("h3");
+let currentNav = document.querySelector("ul");
 
+for (let i = 0; i < currentNav.children.length; i++) {
+  if (currentPage.textContent === currentNav.children[i].textContent) {
+    currentNav.children[i].style.backgroundColor = "#bbdecf";
+  }
+}
