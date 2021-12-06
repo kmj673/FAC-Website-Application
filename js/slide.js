cafdba7132acd2eefcb3d724d0ef8074dccc1944
Minju Kim <kmj673@gmail.com>
2021-12-06 23:29:33 +0000

    set up the slide.js

--- js/slide.js
+++ js/slide.js
@@ -1,1 +1,19 @@
-console.log(`checked`);
+// grab the container for each slide(project 123), left right button
+
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const projects = document.querySelectorAll(".container");
+
+// get which button is clicked,
+
+function clicked(event) {
+  //   console.log(event.target);
+  if (event.target.textContent === ">") {
+    // to check the if the container is the hidden(display visible) or not
+  } else if (event.target.textContent === "<") {
+  }
+}
+
+// right or left is clicked
+right.addEventListener("click", clicked);
+left.addEventListener("click", clicked);
