177a05d2c4e1bef8ecccef71d51577ed82cce958
Minju Kim <kmj673@gmail.com>
2021-12-09 00:05:27 +0000

    added data.js and write pseuod code for popup in weekly prompts tab

---
+++ js/data.js
@@ -0,0 +1,52 @@
+const promptsObj = {
+  week1: {
+    topic: "Functions",
+    platform: "Repl",
+    link: "https://replit.com/@mnmnmnnnnmnmnmm/Week-1-Functions#script.js",
+  },
+  week2: {
+    topic: "Number functions",
+    platform: "Repl",
+    link: "https://replit.com/@mnmnmnnnnmnmnmm/Week-2-Number-functions#script.js",
+  },
+  week3: {
+    topic: "DOM",
+    platform: "StackBlitz",
+    link: "https://stackblitz.com/edit/week-3-dom?file=script.js",
+  },
+  week4: {
+    topic: "Array Methods",
+    platform: "StackBlitz",
+    link: "https://stackblitz.com/edit/week4-array-method-complete?file=script.js",
+  },
+  week5: {
+    topic: "git and GitHub",
+    platform: "VS Code / GitHub",
+    link: "https://github.com/kmj673/week5-git-and-Github",
+  },
+  week6: {
+    topic: "FlexBox",
+    platform: "VS Code / GitHub",
+    link: "https://github.com/kmj673/week6-FlexBox",
+  },
+  week7: {
+    topic: "Building your website",
+    platform: "VS Code / GitHub",
+    link: "https://github.com/kmj673/week7-Building-your-website",
+  },
+  week8: {
+    topic: "Objects",
+    platform: "VS Code / GitHub",
+    link: "https://github.com/kmj673/week8-Objects",
+  },
+  week9: {
+    topic: "Build a feature",
+    platform: "VS Code / GitHub",
+    link: "https://github.com/kmj673/week9-Build-a-feature",
+  },
+  week10: {
+    topic: "Debugging your website",
+    platform: "VS Code / GitHub",
+    link: "https://github.com/kmj673/week10-Debugging-your-website",
+  },
+};
--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,22 @@
-console.log(`checked`);
+//grab the week div
+const week = document.querySelector(".week");
+
+//render each week with promptsObjs key and value
+function eachWeek(week, info) {
+  console.log(week, info);
+}
+//render every week with DOM manipulation
+for (const [week, info] of Object.entries(promptsObj)) {
+  //   console.log(key, value);
+  eachWeek(week, info);
+}
+
+//popup with Document Object Model manipulation.
+
+// create elelement for pop up
+
+// add things with DOM in popup
+
+//link to each week element with addEventListener
+
+// add x to close popup
