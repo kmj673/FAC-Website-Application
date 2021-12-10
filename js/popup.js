cb313aac36f338142d017c7668f8f5dfb68d1c78
Minju Kim <kmj673@gmail.com>
2021-12-10 01:22:51 +0000

    added pop up and link to each week

--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,45 @@
-//grab the week div
-const week = document.querySelector(".week");
-
+//grab the container div
+const weeks = document.querySelectorAll(".week");
+const container = document.querySelector(".container");
+const body = document.querySelector("body");
 //render each week with promptsObjs key and value
-function eachWeek(week, info) {
-  console.log(week, info);
-}
-//render every week with DOM manipulation
-for (const [week, info] of Object.entries(promptsObj)) {
-  //   console.log(key, value);
-  eachWeek(week, info);
+function eachWeek(weekNumber, info) {
+  console.log(weekNumber, info);
 }
 
 //popup with Document Object Model manipulation.
 
-// create elelement for pop up
+//close pop up
+function closePopUp(event) {
+  const popUp = event.target.parentNode;
+  popUp.style.display = "none";
+}
 
-// add things with DOM in popup
+// create  pop up
+const popup = () => {
+  const popUp = document.createElement("div");
+  popUp.classList.add("popUp");
+  container.append(popUp);
+  // add x to close popup
+  const xButton = document.createElement("span");
+  xButton.textContent = "X";
+  xButton.classList.add("xButton");
+  popUp.append(xButton);
+  xButton.addEventListener("click", closePopUp);
+  // add things with DOM in popup
+  return popUp;
+};
 
 //link to each week element with addEventListener
-
-// add x to close popup
+weeks.forEach((week) =>
+  week.addEventListener("click", function () {
+    const popUp = popup();
+    const weekCount = event.target.textContent;
+    //render every week with DOM manipulation
+    for (const [weekNumber, info] of Object.entries(promptsObj)) {
+      if (weekCount === weekNumber) {
+        eachWeek(weekNumber, info);
+      }
+    }
+  })
+);
