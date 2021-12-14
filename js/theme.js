7c299d5e143388fd9819352f15c8cc859daf6850
Minju Kim <kmj673@gmail.com>
2021-12-14 21:49:52 +0000

    changed background color as theme chosen

--- js/theme.js
+++ js/theme.js
@@ -1,12 +1,18 @@
 let dark = document.querySelector(".dark");
 let light = document.querySelector(".light");
 
+//if moon emoji is clicked sun emoji will be appeared.
+//dark theme
 dark.addEventListener("click", () => {
   light.classList.remove("hidden");
   dark.classList.add("hidden");
+  document.body.style.backgroundColor = "#000";
 });
 
+//light theme
+//if sun emoji is clicked moon emoji will be appeared.
 light.addEventListener("click", () => {
   dark.classList.remove("hidden");
   light.classList.add("hidden");
+  document.body.style.backgroundColor = "#fff";
 });
